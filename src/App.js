ad969d8bed46fc97e241410a00c7fe19e48137ed
fynnpeng <514625181@qq.com>
2019-04-24 11:12:55 +0800

    前端秒开计划： react同构直出 + h2 + localstoge + pwa 实现 同构直出 +pwa

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import {Switch} from 'react-router-dom';
+import Home from './Home';
+import './App.css';
+
+
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+  </Switch>
+);
+
+export default App;
